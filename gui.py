5479c19fe043e62b264e4255e4b05f75bb789a28
lureiny <lureniy@gmail.com>
2019-09-21 17:43:46 +0800

    feat(gui.py): 优化GUI模块
    1.注释掉菜单栏，将导入按钮移入主区
    2.注释掉Label
    3.优化鼠标捕捉事件

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-import tkinter as tk 
+import tkinter as tk
 import tkinter.messagebox
 import tkinter.filedialog
 from read_file import ReadExcel
@@ -8,7 +8,7 @@ import os
 class App:
     def __init__(self, root):
         self.root = root
-        self.var = tk.StringVar()
+        # self.var = tk.StringVar()
         self.names = tk.StringVar()
         self.list = None
         self.sign = ""
@@ -16,34 +16,38 @@ class App:
         self.data = None
 
         self.init_window()
-        self.refresh_data()
 
     def init_window(self):
-        self.root.title('My Window')
-        self.root.geometry('1000x500')
+        self.root.title('自动回访')
+        self.root.geometry('600x400')
+        self.root.resizable(width=False, height=False)
 
-        self.var.set("请导入文件")
+        # self.var.set("请导入文件")
 
         # 创建菜单栏
-        menubar = tk.Menu(self.root)
-        filemenu = tk.Menu(menubar, tearoff=0)
-        menubar.add_cascade(label='File', menu=filemenu)
-        filemenu.add_command(label="导入文件", command=self.import_file)
-        self.root.config(menu=menubar)
+        # menubar = tk.Menu(self.root)
+        # filemenu = tk.Menu(menubar, tearoff=0)
+        # menubar.add_cascade(label='File', menu=filemenu)
+        # filemenu.add_command(label="导入文件", command=self.import_file)
+        # self.root.config(menu=menubar)
 
-        frame1 = tk.Frame(self.root, width=200)
+        frame1 = tk.Frame(self.root)
         frame1.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         frame2 = tk.Frame(self.root)
         frame2.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        frame3 = tk.Frame(frame2)
+        frame3.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
-        tk.Label(frame1, textvariable=self.var, font=('Arial', 12), height=2, width=2, bg="white").pack(fill=tk.X, pady=2)
+        # tk.Label(frame1, textvariable=self.var, font=('Arial', 12), height=2, width=2, bg="white").pack(fill=tk.X, pady=2)
         self.list = tk.Listbox(frame1)
         self.list.pack(fill=tk.BOTH, expand=1, pady=2)
+        self.list.bind("<ButtonRelease-1>", self.refresh_data, add=True)
         self.text = tk.Text(frame2)
-        self.text.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
-        self.text.bind_all("<Button-1>", self.refresh_data)
+        self.text.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-        tk.Button(frame2, text="Text", command=self.button).pack(fill=tk.BOTH, expand=1)
+        tk.Button(frame3, text="导入文件", command=self.import_file, height=1, width=10).pack(side=tk.LEFT, expand=1)
+        tk.Button(frame3, text="发送", command=self.button, height=1, width=10).pack(side=tk.LEFT, expand=1)
+        tk.Button(frame3, text="自动发送", command=self.button, height=1, width=10).pack(side=tk.LEFT, expand=1)
 
     def button(self):
         self.generate_applescript(self.data.wechat[self.list.get(self.list.curselection())])
@@ -63,13 +67,14 @@ class App:
             self.list.insert(1, name)
         self.names.set(names)
 
-    def refresh_data(self, *args):
+    def refresh_data(self, event):
         try:
             name = self.list.get(self.list.curselection())
-            self.var.set(name)
+            # self.var.set(name)
         except Exception:
             return -1
         if self.sign != name:
+            self.data.all_info[self.sign] = self.text.get("1.0", tk.END)
             self.sign = name
             self.text.delete("1.0", tk.END)
             self.text.insert(tk.END, self.data.all_info[name])
