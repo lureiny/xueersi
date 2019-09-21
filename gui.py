81c95a67d81348da72f836a4dbbc7fb495101ade
lureiny <lureniy@gmail.com>
2019-09-21 22:01:12 +0800

    feat(gui.py): 优化，完善功能
    1.窗口大小固定并给出初始在屏幕中的位置
    2.完成发送和自动发送的功能 3.对出错细粒化处理
    4.优化多个文件连续导入的功能

--- gui.py
+++ gui.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 import tkinter.messagebox
 import tkinter.filedialog
-from read_file import ReadExcel
+from read_file import ReadExcel, ReadError, GradeError, ComprehensionError, ModelError, WeChatError
 import os
+import openpyxl
 
 
 class App:
     def __init__(self, root):
         self.root = root
         # self.var = tk.StringVar()
-        self.names = tk.StringVar()
         self.list = None
         self.sign = ""
         self.text = None
@@ -19,7 +19,7 @@ class App:
 
     def init_window(self):
         self.root.title('自动回访')
-        self.root.geometry('600x400')
+        self.root.geometry('600x400+%d+%d' % (self.root.winfo_screenwidth()/2 - 300, self.root.winfo_screenheight()/2 - 200))
         self.root.resizable(width=False, height=False)
 
         # self.var.set("请导入文件")
@@ -46,26 +46,65 @@ class App:
         self.text.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
         tk.Button(frame3, text="导入文件", command=self.import_file, height=1, width=10).pack(side=tk.LEFT, expand=1)
-        tk.Button(frame3, text="发送", command=self.button, height=1, width=10).pack(side=tk.LEFT, expand=1)
-        tk.Button(frame3, text="自动发送", command=self.button, height=1, width=10).pack(side=tk.LEFT, expand=1)
+        tk.Button(frame3, text="发送", command=self.send, height=1, width=10).pack(side=tk.LEFT, expand=1)
+        tk.Button(frame3, text="自动发送", command=self.auto_send, height=1, width=10).pack(side=tk.LEFT, expand=1)
 
-    def button(self):
-        self.generate_applescript(self.data.wechat[self.list.get(self.list.curselection())])
+    def send(self):
+        try:
+            self.send_to_one(self.list.get(self.list.curselection()), self.text.get("1.0", tk.END))
+            self.list.itemconfigure(self.list.curselection(), background="red", foreground="white", selectbackground="red")
+        except Exception:
+            tk.messagebox.showerror(title="Error", message="请先导入Excel文件或选择需要发送信息的学生")
+
+    def send_to_one(self, student, info):
+        self.generate_applescript(self.data.wechat[student])
         self.root.clipboard_clear()
-        self.root.clipboard_append(self.text.get("1.0", tk.END))
+        self.root.clipboard_append(info)
         self.run_applescript()
 
+    def auto_send(self):
+        sign = tk.messagebox.askquestion(title='Attention', message="是否开始自动发送信息？")
+        try:
+            if sign == "yes":
+                if self.list.size() == 0:
+                    raise Exception
+                for index in range(self.list.size()):
+                    student = self.list.get(index)
+                    self.send_to_one(student=student, info=self.data.all_info[student])
+                    self.list.itemconfigure(index, background="red", foreground="white", selectbackground="red")
+        except Exception:
+            tk.messagebox.showerror(title="Error", message="请先导入Excel文件")
+
     def import_file(self):
-        filename = tkinter.filedialog.askopenfilename()
+        # 选择文件，限制为xlsx格式
+        filename = tkinter.filedialog.askopenfilename(filetypes=[("Excel 工作簿", "*.xlsx")], title="请选择xlsx文件")
         try:
             self.data = ReadExcel(filename)
-        except Exception as e:
-            tk.messagebox.showwarning(title='Error', message=e.__str__())
+        except ReadError as e:
+            tk.messagebox.showerror(title='Error', message=e.__str__())
             return -1
-        names = ""
+        except GradeError as e:
+            tk.messagebox.showerror(title='Error', message=e.__str__())
+            return -1
+        except ComprehensionError as e:
+            tk.messagebox.showerror(title='Error', message=e.__str__())
+            return -1
+        except ModelError as e:
+            tk.messagebox.showerror(title='Error', message=e.__str__())
+            return -1
+        except WeChatError as e:
+            tk.messagebox.showerror(title='Error', message=e.__str__())
+            return -1
+        except openpyxl.utils.exceptions.InvalidFileException:
+            return -1
+        except Exception as e:
+            tk.messagebox.showerror(title='Error', message=e.__str__())
+            return -1
+
+        # 清空列表
+        self.list.delete(0, self.list.size()-1)
         for name in self.data.grade.keys():
             self.list.insert(1, name)
-        self.names.set(names)
 
     def refresh_data(self, event):
         try:
@@ -79,7 +118,8 @@ class App:
             self.text.delete("1.0", tk.END)
             self.text.insert(tk.END, self.data.all_info[name])
 
-    def generate_applescript(self, wechat):
+    @staticmethod
+    def generate_applescript(wechat):
         script_model = """tell application "WeChat" to activate
 tell application "System Events"
     tell process "WeChat"
@@ -96,7 +136,8 @@ end tell""" % wechat
         with open("temp.applescript", "w") as file:
             file.write(script_model)
 
-    def run_applescript(self):
+    @staticmethod
+    def run_applescript():
         os.system("osascript temp.applescript")
 
 
@@ -105,4 +146,3 @@ if __name__ == '__main__':
     root = tk.Tk()
     app = App(root=root)
     root.mainloop()
-
